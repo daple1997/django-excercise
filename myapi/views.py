649430319c93be10ea5fde31e56345e85db7b2ff
pled2720 <d.pleskach@uleth.ca>
2024-09-16 23:22:11 -0600

    adding a view for the front end build

--- myapi/views.py
+++ myapi/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Item
 from .serializers import ItemSerializer
+from django.shortcuts import render
+
 
 class ItemList(APIView):
     def get(self, request):
@@ -19,3 +21,6 @@ class ItemList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def index(request):
+        return render(request, 'frontend/build/index.html')
