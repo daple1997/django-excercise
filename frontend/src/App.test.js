cd2d9630f6be04b8e169b089aca028303a827c8b
pled2720 <d.pleskach@uleth.ca>
2025-03-20 22:44:16 -0600

    Adding mods and tests for axios

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,13 @@
+import axios from "axios";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 jest.mock("axios");
+
+test("axios mock test", () => {
+  axios.get.mockResolvedValue({ data: { hello: "world" } });
+  expect(axios.get).toBeCalledTimes(0); // hasn't been called yet
+});
 
 test("renders learn react link", () => {
   render(<App />);
