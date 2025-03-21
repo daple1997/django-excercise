0b10246cfe8668df354d7516d30b520bb26e0890
pled2720 <d.pleskach@uleth.ca>
2025-03-20 22:58:00 -0600

    Adding a mock to accomodate Antd

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,9 +4,27 @@ import App from "./App";
 
 jest.mock("axios");
 
+// Mock matchMedia (fix for Ant Design's grid responsiveness)
+beforeAll(() => {
+  window.matchMedia =
+    window.matchMedia ||
+    function () {
+      return {
+        matches: false,
+        media: "",
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      };
+    };
+});
+
 test("axios mock test", () => {
   axios.get.mockResolvedValue({ data: { hello: "world" } });
-  expect(axios.get).toBeCalledTimes(0); // hasn't been called yet
+  expect(axios.get).toBeCalledTimes(0);
 });
 
 test("renders learn react link", () => {
