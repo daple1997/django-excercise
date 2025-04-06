0a581574fbb09e2682e9ec5c4cc383460fbbb4c0
pled2720 <d.pleskach@uleth.ca>
2025-04-05 21:06:27 -0600

    Mocking react-router-dom to prevent jest failtures

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+// Mock react-router-dom to avoid Jest trying to resolve it
+jest.mock("react-router-dom", () => ({
+  BrowserRouter: "div",
+  Route: "div",
+  Routes: "div",
+}));
