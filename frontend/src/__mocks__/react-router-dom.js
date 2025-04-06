7475944c695f74877321c66a87753b2d42de4c53
pled2720 <d.pleskach@uleth.ca>
2025-04-05 21:21:19 -0600

    This is to mock react-router-dom

---
+++ frontend/src/__mocks__/react-router-dom.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BrowserRouter: "div",
+  Route: "div",
+  Routes: "div",
+};
--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -4,6 +4,7 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 // Mock react-router-dom to avoid Jest trying to resolve it
+
 jest.mock("react-router-dom", () => ({
   BrowserRouter: "div",
   Route: "div",
